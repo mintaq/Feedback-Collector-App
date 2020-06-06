0b4b9bb9c293d4eb1c7b81ac83b8932092c08f19
mintaq <minh.tranquang98@gmail.com>
2020-06-06 10:17:27 +0700

    Update UI

---
+++ server/client/src/components/surveys/SurveyList/SurveyList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../../store/actions';
+import SurveyCard from '../SurveyCard';
+import { Card } from 'antd';
+import styles from './SurveyList.module.css';
+
+class SurveyList extends Component {
+	async componentDidMount() {
+		await this.props.fetchSurveys();
+	}
+
+	renderSurveys() {
+		if (this.props.surveys.length === 0) {
+			return (
+				<div className={styles.CardContainer}>
+					<Card style={{backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
+					<p>	Hiện bạn chưa tạo một khảo sát nào. Bấm vào nút tạo ở góc dưới bên phải để tạo khảo sát mới!</p>
+					</Card>
+				</div>
+			);
+		}
+
+		return this.props.surveys.reverse().map(survey => <SurveyCard key={survey._id} {...survey} />);
+	}
+
+	render() {
+		return <div className={styles.container}>{this.renderSurveys()}</div>;
+	}
+}
+
+const mapStateToProps = state => {
+	return { surveys: state.surveys.surveys };
+};
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
